eb1d89ff40cbbe421befd858d6fc6c603f74eaaa
Bernardo-Junior <bernardojunior540@gmail.com>
2020-05-25 16:16:00 -0400

    Adicionando backend

---
+++ [front-end-Bernardo]/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, StatusBar, Text, View , YellowBox} from 'react-native';
+
+StatusBar.setBackgroundColor('#A7CE03');
+StatusBar.setBarStyle('dark-content');
+
+YellowBox.ignoreWarnings([
+  'should never be nested inside plain',
+  'componentWillMount is deprecated and will be removed in the next major version',
+  'componentWillReceiveProps has been renamed, and is not recommended for use',
+  'componentWillMount has been renamed, and is not recommended for use'
+])
+
+
+import Routes from './src/router';
+
+export default function App() {
+  return (
+   <Routes />
+  );
+}
---
+++ [front-end-Bernardo]/src/router.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { 
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as loc,
+  removeOrientationListener as rol
+} from 'react-native-responsive-screen';
+
+import Home from './pages/home';
+import Sobre from './pages/sobre';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'ios-home' : 'ios-home';
+                          }  else {
+                            iconName = focused ? 'md-help-circle-outline' : 'md-help-circle-outline';
+                          }
+                          // You can return any component that you like here!
+                          return <Ionicons name={iconName} size={25} color={color} />;
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: '#FFF',
+                        inactiveTintColor: '#BFE6B3',
+                        style: {
+                          backgroundColor: '#7EC24D',
+                          height: hp('7%')
+                        }
+                      }}
+            
+            >
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="Sobre" component={Sobre} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
